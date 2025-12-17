632c8068b3a1d1d570a5041d619aaf7a7f75cd83
mike <706420258@qq.com>
2025-12-17 15:42:41 +0000

    fix

---
+++ main.py
@@ -0,0 +1,26 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@dag(
+    dag_id="creator_metric_length_check",
+    schedule="0 10 * * *",
+    max_active_runs=1,
+    catchup=False,
+    tags=["mike"],
+    start_date=datetime(2025, 12, 16),
+)
+def creator_metric_length_check():
+
+    @task
+    def check_length():
+        logger.info("Hello Airflow 3!")
+        return "Hello Airflow 3!"
+
+    check_length()
+
+
+creator_metric_length_check_dag = creator_metric_length_check()
